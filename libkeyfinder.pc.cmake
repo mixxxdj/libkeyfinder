4d7d75d0065854fa7d9ed9e4ad986de403482331
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-01-14 14:12:04 +0100

    CMake: Move version into project() call

--- libkeyfinder.pc.cmake
+++ libkeyfinder.pc.cmake
@@ -4,7 +4,7 @@ libdir=${prefix}/@CMAKE_INSTALL_LIBDIR@
 
 Name: libKeyFinder
 Description: libKeyFinder can be used to estimate the musical key of digital audio
-Version: @LIBKEYFINDER_VERSION@
+Version: @CMAKE_PROJECT_VERSION@
 URL: https://github.com/ibsh/libKeyFinder
 Libs: -L${libdir} -lkeyfinder
 Cflags: -I${includedir}
